d1504ca6e9d1df38f6aabc8575372eb62d310c9a
farkaszsombor <fzsombor93@yahoo.com>
2017-11-07 15:44:37 +0200

    Added Login & Register

--- app/src/main/java/com/example/zsombor/bus_up/MainActivity.java
+++ app/src/main/java/com/example/zsombor/bus_up/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.zsombor.bus_up;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,21 +10,22 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mButon;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mButon = (Button)findViewById(R.id.sign_out);
 
-        mButon.setOnClickListener(new View.OnClickListener() {
+        mButton = (Button)findViewById(R.id.sign_out);
+        mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FirebaseAuth auth = FirebaseAuth.getInstance();
-                auth.signOut();
+                FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                mAuth.signOut();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
             }
         });
     }
-    //this is some text
 }
