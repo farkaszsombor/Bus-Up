0781d27016dac3e9a04300ccce04203a7a74f21f
farkaszsombor <fzsombor93@yahoo.com>
2017-11-07 10:33:34 +0200

    Added Login & Register

--- app/src/main/java/com/example/zsombor/bus_up/MainActivity.java
+++ app/src/main/java/com/example/zsombor/bus_up/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.zsombor.bus_up;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button;
+        mButon = (Button)findViewById(R.id.sign_out);
+
+        mButon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth auth = FirebaseAuth.getInstance();
+                auth.signOut();
+            }
+        });
     }
 }
