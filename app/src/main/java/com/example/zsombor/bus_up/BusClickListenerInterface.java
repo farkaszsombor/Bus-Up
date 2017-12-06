7985c7d2e5849b2b0237c732fa021da33c8dd36f
farkaszsombor <fzsombor93@yahoo.com>
2017-12-06 16:51:16 +0200

    Route Drawing Added

---
+++ app/src/main/java/com/example/zsombor/bus_up/BusClickListenerInterface.java
@@ -0,0 +1,11 @@
+package com.example.zsombor.bus_up;
+
+import android.view.View;
+
+/**
+ * Created by Zsombor on 12/1/2017.
+ */
+
+public interface BusClickListenerInterface {
+    public void onItemClick(View view,int position);
+}
